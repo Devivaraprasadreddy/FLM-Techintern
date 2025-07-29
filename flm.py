4fa729fc57586d8e9117c43284a1e1e817388530
reddy <misrithajyothi@gmail.com>
2025-07-29 10:45:38 +0530

    flm files added

---
+++ flm.py
@@ -0,0 +1,8 @@
+# Input list of email subjects
+emails = [New Lead from Form, Daily Report, Zoom Invite, Cybersecurity Lead]
+
+# Iterate and filter subjects containing lead
+for subject in emails:
+    if lead in subject.lower():
+        print(subject)
+
